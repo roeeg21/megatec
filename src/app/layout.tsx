16de844cbd46063aec97285cf2663f7b57960b0d
רועי גץ <roee.getz@gmail.com>
2025-12-15 19:01:02 +0000

    do the background white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="he" dir="rtl" suppressHydrationWarning>
+    <html lang="he" dir="rtl">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
